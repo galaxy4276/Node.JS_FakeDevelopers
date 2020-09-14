f882b83d37f57f73f8c7a3139eb08b3bb5b7e05f
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-14 17:20:05 +0900

    0914/[test]/DB에 가사용자 100명 생성 함수 작성

---
+++ server/src/models/userQuery.test.js
@@ -0,0 +1,15 @@
+import sequelize from './';
+
+const { User } = sequelize;
+
+async function createBulkUsers() {
+  let id = 1;
+  const password = 'thisUserisTest';
+
+  for (let i = 0; i <= 100; i++) {
+    await User.create({ id, password });
+    id++;
+  }
+}
+
+export default createBulkUsers;
