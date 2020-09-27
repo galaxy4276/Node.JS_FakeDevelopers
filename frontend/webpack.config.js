e2c3166bf59a46147bb9113082daad333402054e
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-27 23:19:32 +0900

    feat: pull 누락 관련 컨플릭트 해결

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "build");
 
 // pug에서 html로 컴파일을 해주면 그 html에 스크립트 코드를 추가하는 함수
 // 아래 html-webpack-plugin 플러그인에서 이 함수 사용
-let htmlPageNames = ["index", "foobar", "fooooo"];
+let htmlPageNames = ["index"];
 let multipleHtmlPlugins = htmlPageNames.map((name) => {
   return new HtmlWebpackPlugin({
     template: `${OUTPUT_DIR}/html/${name}.html`, // relative path to the HTML files
@@ -70,8 +70,6 @@ const webpackConfig = {
     // 용도에 따라 js파일을 구분하고,
     // js 파일을 html 페이지에 별로 청크를 분리하여 작성
     index: path.resolve(__dirname, "src", "es6", "pages", "index.js"),
-    foobar: path.resolve(__dirname, "src", "es6", "pages", "foobar.js"),
-    fooooo: path.resolve(__dirname, "src", "es6", "pages", "fooooo.js"),
     // example1: [path.resolve(__dirname, "src", "assets", "es6", "ex1.js")
     // example2: path.resolve(__dirname, "src", "assets", "es6", "ex2.js"),
     // 각 호출 파일 내부에선 기능별 js를 import
@@ -139,7 +137,7 @@ const webpackConfig = {
     new MiniCssExtractPlugin({
       filename: "css/[name].css",
     }),
-  ], //.concat(verifyHtmlFiles() ? multipleHtmlPlugins : []), // pug에서 컴파일되어 나온 html 파일별로 스크립트 코드 주입하여 출력, 웹팩을 watch 모드로 실행시 변경
+  ].concat(verifyHtmlFiles() ? multipleHtmlPlugins : []), // pug에서 컴파일되어 나온 html 파일별로 스크립트 코드 주입하여 출력, 웹팩을 watch 모드로 실행시 변경
 };
 
 module.exports = webpackConfig;
