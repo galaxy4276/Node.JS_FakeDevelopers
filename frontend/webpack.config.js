a6b66dd2fbc9acedcc16ec5c0e04ffa95a1b7ef8
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-28 14:17:30 +0900

    feat: 웹팩 출력 경로를 하나 추가, 서버단의 작업 디렉터리로 설정
     server 디렉터리 내부에 생성되도록 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -156,7 +156,140 @@ const webpackConfig = {
   ].concat(verifyHtmlFiles() ? multipleHtmlPlugins : []), // pug에서 컴파일되어 나온 html 파일별로 스크립트 코드 주입하여 출력, 웹팩을 watch 모드로 실행시 변경
 };
 
-module.exports = webpackConfig;
+// const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+
+const DEV_SERVER_DIR_NAME = "public";
+const DEV_SERVER_DIR = path.resolve(
+  __dirname,
+  `../server/${DEV_SERVER_DIR_NAME}`
+);
+
+// let multipleHtmlPluginsPug = pageNames.map((name) => {
+//   return new HtmlWebpackPlugin({
+//     template: `${DEV_SERVER_DIR}/pages/${name}/${name}.pug`, // relative path to the HTML files
+//     filename: `pages/${name}/${name}.pug`, // server output HTML files
+//     chunks: [`${name}`], // respective JS files
+//   });
+// });
+
+const _webpackConfig_dev_server = {
+  mode: MODE,
+
+  devServer: {
+    contentBase: `${OUTPUT_DIR}/pages`,
+    // contentBase: `${OUTPUT_DIR}/pages/index`,
+    publicPath: "/",
+    overlay: true,
+    port: 3000,
+    hot: true,
+    inline: true,
+    open: true,
+    stats: "errors-only",
+    host: "localhost",
+  },
+
+  devtool: "inline-source-map",
+  // The side effect of this option is to increase build time
+  // mode development ? ‘inline-source-map" : 'hidden-source-map’
+
+  entry: {
+    /*
+    용도에 따라 js파일을 구분하고,
+     js 파일을 html 페이지에 별로 청크를 분리하여 작성
+     example1: path.resolve(__dirname, "src", "assets", "es6", "ex1.js")
+     example2: path.resolve(__dirname, "src", "assets", "es6", "ex2.js"),
+     각 호출 파일 내부에선 기능별 js를 import
+    */
+
+    // for prod
+    index: path.resolve(__dirname, "src", "es6", "pages", "index.js"),
+
+    // for dev
+    __dev_bear: path.resolve(__dirname, "src", "es6", "pages", "__dev_bear.js"),
+    __dev_wscrg: path.resolve(
+      __dirname,
+      "src",
+      "es6",
+      "pages",
+      "__dev_wscrg.js"
+    ),
+  },
+
+  output: {
+    //  entry 에서 분리한 청크별로 다른 번들파일 출력
+    path: DEV_SERVER_DIR,
+    filename: "pages/[name]/[name].js", // 작업예약 200916: 청크해쉬 추가하고 html-webpack-plugin에서 지정하기!!
+    publicPath: "../../",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          "file-loader?name=pages/[name]/[name].html",
+          "extract-loader",
+          "html-loader",
+          "pug-html-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+          // {
+          //   loader: `babel-loader?name=[name]/[name].css`,
+          //   options: {
+          //     presets: ["@babel/preset-env"],
+          //   },
+          // },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192, // (file-size > limit) ? use file-loader
+              publicPath: "./[name]/",
+              name: "img/[name].[ext]?[hash]", //  (mode == "production") ? name: "../img/[hash].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "font/[name].[ext]?[hash]", //  (mode == "production") ? name: "../img/[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "pages/[name]/[name].css",
+    }),
+  ].concat(verifyHtmlFiles() ? multipleHtmlPlugins : []), // pug에서 컴파일되어 나온 html 파일별로 스크립트 코드 주입하여 출력, 웹팩을 watch 모드로 실행시 변경
+};
+//.concat(multipleHtmlPluginsPug)
+
+module.exports = [webpackConfig, _webpackConfig_dev_server];
 
 // module.exports = (env, argv) => {
 //   if (argv.mode === "development") {
