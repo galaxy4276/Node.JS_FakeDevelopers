018adfa05a05156ee061dd883cf66940158758a7
galaxy4276 <galaxyhi4276@gmail.com>
2020-10-03 08:40:45 +0900

    feat: passport 인증 모듈 디렉터리 모듈로 구현, 로컬 로그인 모듈 분리

---
+++ server/src/controllers/passport/index.js
@@ -0,0 +1,27 @@
+import passport from 'passport';
+import sequelize from '../../models';
+import local from './local';
+
+const { User } = sequelize;
+
+
+
+passport.serializeUser(({ id }, done) => {
+  done(null, id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  await User.findByPk(id)
+    .then(user => done(null, user.id))
+    .catch(err => done(null, false, { message: err }));
+}) ;
+
+/*
+  구성해둔 전략 (함수) 를 실행
+  arguments
+  1: passport Module
+  2: sequelize Schema
+*/
+local(passport, User);
+
+export default passport;
