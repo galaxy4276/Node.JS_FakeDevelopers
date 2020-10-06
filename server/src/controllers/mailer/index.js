ca9c8c2fe45b7a6b0dc66d9c038dbd480a9b073b
galaxy4276 <galaxyhi4276@gmail.com>
2020-10-06 16:45:34 +0900

    fix: 메일 시스템 보안사항 수정 및 코드 리팩토링

--- server/src/controllers/mailer/index.js
+++ server/src/controllers/mailer/index.js
@@ -1,23 +1,20 @@
 import nodemailer from 'nodemailer';
-import sequelize from 'sequelize';
+import url from 'url';
+require('dotenv').config();
 
 
 const transEmail = async (req, res) => {
   const { email } = req.body;
-  console.log(`email: ${email}`);
 
   try {
-    const testAccount = nodemailer.createTestAccount();
-    console.log(testAccount);
-  
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       host: 'smtp.gmail.com',
       port: 587,
       secure: false,
       auth: {
-        user: 'galaxyhi4276@gmail.com',
-        pass: 'kneyianlbpjssums',
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
       },
       tls: {
         rejectUnauthorized: false,
@@ -25,10 +22,14 @@ const transEmail = async (req, res) => {
     });
   
     const info = {
-      from: `"test mail" <galaxyhi4276@gmail.com>`,
+      from: `"DDCComputer" ${process.env.EMAIL_USER}`,
       to: email,
-      subject: 'Test',
-      html: '<b>Test!!!!</b>',
+      subject: '광화문 집회...문재인 정부 이대로 가면',
+      html: `
+      <img src="https://image.fmkorea.com/files/attach/new/20200616/486616/1656451610/2949357783/00c58fbff364944d81103e4aed32dbde.jpeg" />
+      <h1>
+        라이더!
+      </h1>`,
     };
 
     await transporter.sendMail(info, (err, res) => {
@@ -42,7 +43,14 @@ const transEmail = async (req, res) => {
       }
       transporter.close();
     });
-  } catch (err) {
+    
+    res.redirect(url.format({
+      pathname: '/',
+      query: {
+        "success": "email_passed"
+      },
+    }));
+    } catch (err) {
     console.log('nodemailer error');
     console.error(err);
   }
