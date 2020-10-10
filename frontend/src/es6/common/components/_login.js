1be847c997be0b5d673a3f18b919e8c303734457
bearbearbear <ghlwkdthfl@gmail.com>
2020-10-11 08:09:17 +0900

    feat: 로그인 컴포넌트 여닫기 기능 추가

--- frontend/src/es6/common/components/_login.js
+++ frontend/src/es6/common/components/_login.js
@@ -3,8 +3,19 @@
 // TODO: X 버튼 누르면 로그인 화면 꺼지도록 설정
 // TODO: 로그인 화면 눌렀을 때에서야 로그인 관련 스크립트 태그를 다운로드 하도록 설정 (미리 하지 말기)
 
-const headerLogin = document.getElementById("#headerLoginBtn"); // header의 Logiun
+const loginOpenBtns = document.querySelectorAll(".js-loginBtn"); // 로그인 열기 버튼
+const loginCloseBtn = document.querySelector(".login__form__close-btn"); // 로그인 닫기 버튼
+const loginWindow = document.querySelector(".login"); 
 
-function onLoginWindow() {}
+function toggleLoginWindow(){
+    loginWindow.classList.toggle("login--show");
+}
 
-// login.addEventListener("click", onLoginWindow, false);
+function init() {
+    loginOpenBtns.forEach((loginOpenBtn) => {
+        loginOpenBtn.addEventListener("click", toggleLoginWindow);
+    });
+    loginCloseBtn.addEventListener("click", toggleLoginWindow);
+}
+
+init();
