54b4710178124404a3d584ea9b7da1c774f59f35
bearbearbear <ghlwkdthfl@gmail.com>
2020-10-12 07:47:40 +0900

    feat: 비밀먼호 찾기 페이지 js 미적용 문제 해결

--- frontend/src/es6/import/auth/hashCheck.js
+++ frontend/src/es6/import/auth/hashCheck.js
@@ -1,17 +1,14 @@
 /* -- find 폼의 input 요소 클릭시 label 포지션 변경 후 그 자리에 고정시키는 이벤트 --*/
 
-const findFormInputs = document.querySelectorAll(".find__form__input");
-
-
-function next(element) {
-    // nextSibling 의 줄바꿈 요소(undifined 요소) 참조를 무시해주는 함수
-    do {
-        element = element.nextSibling;
-    } while (element && element.nodeType !== 1);
-    return element;        
-}
-
 function raiseLabel(e) {
+    function next(element) {
+        // nextSibling 의 줄바꿈 요소(undifined 요소) 참조를 무시해주는 함수
+        do {
+            element = element.nextSibling;
+        } while (element && element.nodeType !== 1);
+        return element;        
+    }
+    
     let input = e.target;
     let label = next(input);
 
@@ -23,10 +20,8 @@ function raiseLabel(e) {
     label.style.color = " #bebebe";
 }
 
-function findFormInputEventHandle(){
-    findFormInputs.forEach((findFormInput) => {
-        findFormInput.addEventListener("click", raiseLabel, {once: true});
-    });
+window.onload = () => {
+    const findFormInput = document.querySelector(".find__form__input");
+    
+    findFormInput.addEventListener("click", raiseLabel, {once: true});
 }
-
-findFormInputEventHandle();
