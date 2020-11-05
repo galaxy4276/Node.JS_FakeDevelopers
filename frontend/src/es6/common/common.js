e89120747d853a26271ea245562ed2a0bd7fee63
Ji-Yong <wldyd2356@gmail.com>
2020-11-05 16:28:43 +0900

    feat: 헤더 드롭다운 리스트 증가 시 드롭다운 Height가 자동 증가하도록 변경

--- frontend/src/es6/common/common.js
+++ frontend/src/es6/common/common.js
@@ -2,9 +2,10 @@
 console.log(`[LOG] 자바스크립트 적용 완료`);
 
 /* -- common auth -- */
-import "./auth/_auth.js";
+import './auth/_auth.js';
 
 /* -- common templates -- */
-import "./templates/_sidebar";
+import './templates/_sidebar';
+import './templates/_header';
 
-/* -- common function -- */ 
+/* -- common function -- */
