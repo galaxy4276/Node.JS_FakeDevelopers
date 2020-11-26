d87238a23396d26e7479342b88ba3edadec2cb51
bearbearbear <bear-bear-bear@kakao.com>
2020-11-27 03:19:24 +0900

    test: 게시판 페이지 테스트 코드 수정

--- frontend/src/es6/import/community/board.js
+++ frontend/src/es6/import/community/board.js
@@ -4,23 +4,24 @@ const post = postList.querySelector('.post-list__post');
 
 /* Test Code for set default style in CSS*/
 const setFakeData = (items) => {
-  const name = items.length === 5 ? 'notice' : 'post';
+  const name = items[0].classList[1].match(/(?<=__).*(?=__)/)[0]; // 'notice' || 'post'
 
   const exData = {
-    number: 1234,
+    number: name === 'notice' ? '[공지사항]' : 1234,
     title: name === 'notice' ? '공지사항입니다.' : '유저 게시글입니다.',
     writer: name === 'notice' ? 'admin' : 'Anonymous',
-    hit: 1234,
+    hit: name === 'notice' ? 12345 : 1234,
     regDate: '0000-00-00',
   };
 
   items.forEach((item) => {
     const props = Array.from(item.children);
     const data = exData;
-    const keys = Object.keys(data);
 
     props.forEach((prop, i) => {
-      prop.textContent = data[keys[i]];
+      let propName = prop.classList[0].match(/(?<=^.*__.*__).*$/)[0]; // 'number' || 'title' || 'writer' || 'hit' || 'reg-date'
+      if (propName === 'reg-date') propName = 'regDate';
+      prop.textContent = data[propName];
     });
   });
 };
