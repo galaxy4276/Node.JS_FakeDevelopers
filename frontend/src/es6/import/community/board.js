a7b8b2477509c6c18caf6a2cede5df60cf1b8d9d
bearbearbear <bear-bear-bear@kakao.com>
2020-11-28 03:28:51 +0900

    feat: 자유이야기 게시판 postList에 대한 데이터 요청 함수 작성

--- frontend/src/es6/import/community/board.js
+++ frontend/src/es6/import/community/board.js
@@ -1,3 +1,36 @@
-import { setFakeData } from '../__dev/bear';
+/* Create Temporary Text */
+// import { setFakeData } from '../__dev/bear';
+// document.addEventListener('DOMContentLoaded', setFakeData, false);
+// document.addEventListener('DOMContentLoaded', () => setPostList(url), false);
+/* --------------------- */
+/* Variables */
+const LIMIT = 15;
+// let currentPage = 1;
 
-document.addEventListener('DOMContentLoaded', setFakeData, false);
+/* Function */
+const getPostList = (url = '') => {
+  return fetch(url, {
+    method: 'GET',
+    // mode: process.env.DEV_MODE === 'development' ? 'no-cors' : 'same-origin',
+    // credentials: process.env.DEV_MODE === 'development' ? 'same-origin' : 'include', // 조회수 검증을 위한 쿠키 허용
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => res.json());
+};
+
+const setPostList = () => {
+  console.log(`요청 url => http://localhost:8001/community?limit=${LIMIT}&page=1`); //test
+
+  getPostList(`http://localhost:8001/community?limit=${LIMIT}`)
+    .then((res) => {
+      // test
+      console.log(`받아온 데이터 개수 => ${res.length}`);
+      return res;
+    })
+    .then((data) => JSON.stringify(data))
+    .catch(console.error);
+};
+
+document.addEventListener('DOMContentLoaded', setPostList, false);
