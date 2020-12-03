d32120af02ca89e69f68e97f47d9be3b0612f253
galaxy <galaxyhi4276@gmail.com>
2020-12-03 21:58:10 +0900

    feat: 전체 페이지 요청 API 작성

--- server/src/controllers/pagination/getPageIdx.js
+++ server/src/controllers/pagination/getPageIdx.js
@@ -1,10 +1,21 @@
-const getPageIdx = (res, next) => async schema => {
+import { reqValidAndDefault } from '../index';
+
+const getPageIdx = (req, res, next) => async schema => {
   try {
+    const limit = reqValidAndDefault(req.query.limit, 15);
+
+    if (!limit) {
+      throw Error("페이지 총 인덱스를 불러올 수 없습니다.");
+    }
+
     const allPageIdx = await schema.findAll({
       attributes: ['id'],
     });
 
-    res.status(200).send(allPageIdx.length);
+    const idx = allPageIdx.length / limit;
+    console.log(`idx: ${idx}`);
+    
+    return res.status(200).json({ idx: idx });
   } catch (err) {
     console.log('allPageIdx ERROR');
     console.error(err);
@@ -12,4 +23,5 @@ const getPageIdx = (res, next) => async schema => {
     next(err);
   }
 }
-  
+
+export default getPageIdx;
