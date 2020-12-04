10a2e852df49a2a69426c3a9667a6619b312e1de
bearbearbear <bear-bear-bear@kakao.com>
2020-12-05 01:47:53 +0900

    test: createBulk 코드에서 생성 데이터 수를 랜덤한 값으로 받도록 설정

--- frontend/src/es6/common/function/post/createBulk.js
+++ frontend/src/es6/common/function/post/createBulk.js
@@ -1,9 +1,15 @@
 // https://www.ddccomputer.club/[게시판 이름]/api/create-bulk?count=60
+// 위 API로 게시판에 가상 게시글 형성 가능
 const allPath = {
   intro: ['info', 'member', 'env', 'club'],
   milestone: ['curriculum', 'cert', 'career'],
   community: ['board', 'suggestion', 'donation', 'notice'],
   footprint: ['acquisition', 'awards', 'portfolio'],
+};
+
+const currTestPath = {
+  // 원한다면 이렇게 하나의 게시판만 넣을수도 있음
+  community: ['board'],
 };
 
 const makeURLs = (path) => {
@@ -12,8 +18,10 @@ const makeURLs = (path) => {
   for (const category in path) {
     const boards = path[category];
 
+    const randomNum = Math.floor(Math.random() * 300);
+
     const urls = boards.map(
-      (board) => `http://localhost:8001/${category}/${board}/api/create-bulk?count=320`
+      (board) => `http://localhost:8001/${category}/${board}/api/create-bulk?count=${randomNum}`
     );
 
     allURL.push(urls);
@@ -23,7 +31,7 @@ const makeURLs = (path) => {
 };
 
 const createBulk = () => {
-  const requestURLs = makeURLs(allPath);
+  const requestURLs = makeURLs(currTestPath);
 
   requestURLs.forEach((url) => {
     fetch(url)
