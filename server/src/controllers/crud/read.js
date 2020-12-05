d87670c16a8e76e416332a2158a3e019df5c025d
galaxy <galaxyhi4276@gmail.com>
2020-12-06 04:06:33 +0900

    feat: read 기능 수정

--- server/src/controllers/crud/read.js
+++ server/src/controllers/crud/read.js
@@ -1,23 +1,27 @@
-const readPost = (req, res, next) => async schema => {
-  try {
-    const { id } = req.params;
+const readPost = (req, res, next) => {
+  return async schema => {
+    try {
+      console.log('readPost');
 
-    const redirectUrl = req.originalUrl
-      .match(/[a-z]+\/[a-z]+/g)
-      .join('');
+      const { id } = req.params;
 
-    const post = await schema.findOne({
-      where: { id },
-      // need add Comments
-    });
+      const redirectUrl = req.originalUrl
+        .match(/\/[a-z]+\/[a-z]+/g)
+        .join('');
 
-    // req.originalUrl 로 대체가 가능해 보임
-    res.render(`import${redirectUrl}`, { post });
-  } catch(err) {
-    console.log('acquisitionPost Error');
-    console.error(err);
-    next(err);
-  }
+      const post = await schema.findOne({
+        where: { id },
+        // need add Comments
+      });
+
+      // req.originalUrl 로 대체가 가능해 보임
+      res.render(`import${redirectUrl}`, {post});
+    } catch (err) {
+      console.log('acquisitionPost Error');
+      console.error(err);
+      next(err);
+    }
+  };
 }
 
 export default readPost;
