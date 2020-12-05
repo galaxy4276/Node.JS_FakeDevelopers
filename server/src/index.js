ac11dd263cde3152c537ebe0776387b17d942e47
galaxy <galaxyhi4276@gmail.com>
2020-12-06 04:06:22 +0900

    feat: 개발 / 배포 로그 환경

--- server/src/index.js
+++ server/src/index.js
@@ -46,7 +46,13 @@ app.set('views', path.resolve(__dirname, 'public', 'views')); // view 디렉터�
 
 app.use(cors()); // Cross Origin 문제 해결 미들웨어
 app.use(helmet()); // 보안 관련 미들웨어
-app.use(morgan('common')); // 서버 로깅
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // 서버 로깅
+} else {
+  app.use(morgan('combined')); // 서버 로깅
+}
+
 app.use('/css', express.static(routes.frontCss)); // 프론트 CSS 파일 위치
 app.use('/es5', express.static(routes.frontEs6)); // 프론트 자바스크립트 파일 위치
 app.use('/img', express.static(routes.frontImg)); // 프론트 이미지파일 위치
@@ -61,13 +67,15 @@ app.use(cookieParser(process.env.secret)); // 쿠키 생성 관련 미들웨어
 app.use(
   session({
     secret: process.env.secret,
+    proxy: true,
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
     cookie: {
       httpOnly: true,
-      secure: false,
-      expires: new Date(Date.now() + 1800000), 
+      secure: true,
+      expires: new Date(Date.now() + 800000),
+      maxAge: 800000,
     },
   })
 ); /*
