de0f5a3369683007c65904fd7efabca29cec8eeb
bearbearbear <bear-bear-bear@kakao.com>
2020-12-06 00:49:55 +0900

    feat: postList를 entry에서 제거하고 export 함수 이름 변경
    paginatePostList() -> postList()

--- frontend/src/_entry/post/postList.js
+++
@@ -1,9 +0,0 @@
-/* -- pug --*/
-
-/* -- scss --*/
-import '../../scss/common/post/_postList.scss';
-
-/* -- javascript -- */
-import '../../es6/common/post/postList/paginatePostList';
-
-/* -- font -- */
