f4bc41d1ed00aefa316e0aebe1eb0b266eb42f4d
bearbearbear <bear-bear-bear@kakao.com>
2020-12-06 02:10:27 +0900

    feat: 게시글 쓰기, 상세보기 기능이 불필요하다 판단되는 카테고리들에서 게시글 쓰기, 상세보기 템플릿 삭제

--- frontend/src/_entry/post/postView.js
+++ frontend/src/_entry/post/postView.js
@@ -1,8 +1,6 @@
 /* -- pug --*/
 import '../../views/import/community/postView.pug';
 import '../../views/import/footprint/postView.pug';
-import '../../views/import/intro/postView.pug';
-import '../../views/import/milestone/postView.pug';
 
 /* -- scss --*/
 import '../../scss/common/post/_postView.scss';
--- frontend/src/_entry/post/postWrite.js
+++ frontend/src/_entry/post/postWrite.js
@@ -1,7 +1,6 @@
 /* -- pug --*/
 import '../../views/import/community/create.pug';
 import '../../views/import/footprint/create.pug';
-import '../../views/import/milestone/create.pug';
 
 /* -- scss --*/
 import '../../scss/common/post/_postWrite.scss';
