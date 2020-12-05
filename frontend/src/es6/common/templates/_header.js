0a380e03d13525c160d98e23e745384a6c3c0474
bearbearbear <bear-bear-bear@kakao.com>
2020-12-06 07:23:08 +0900

    fix: 로그아웃 버튼 404 에러 수정

--- frontend/src/es6/common/templates/_header.js
+++ frontend/src/es6/common/templates/_header.js
@@ -1,19 +1,41 @@
-const dropdownMenuList = document.querySelectorAll(
-  '.header-menu-dropdown__list'
-);
+const initDropDownListHeight = () => {
+  const dropdownMenuList = document.querySelectorAll('.header-menu-dropdown__list');
 
-const defaultHeight = 48;
+  const defaultHeight = 48;
 
-const findMaximumMenuList = () => {
-  let max = 0;
+  const findMaximumMenuList = () => {
+    let max = 0;
+    dropdownMenuList.forEach((h) => {
+      if (max < h.children.length) max = h.children.length;
+    });
+    return max;
+  };
+
+  let dropdownMenuMax = findMaximumMenuList();
+
   dropdownMenuList.forEach((h) => {
-    if (max < h.children.length) max = h.children.length;
+    h.style.height = dropdownMenuMax * defaultHeight + 'px';
   });
-  return max;
 };
 
-let dropdownMenuMax = findMaximumMenuList();
+const initLogoutBtnHref = () => {
+  const logoutBtn = document.body.querySelector('.header-util__link--logout');
 
-dropdownMenuList.forEach((h) => {
-  h.style.height = dropdownMenuMax * defaultHeight + 'px';
-});
+  if (!logoutBtn) return;
+
+  const host =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8001'
+      : 'https://www.ddccomputer.club';
+
+  logoutBtn.setAttribute('href', `${host}/auth/logout`);
+};
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    initDropDownListHeight();
+    initLogoutBtnHref();
+  },
+  false
+);
