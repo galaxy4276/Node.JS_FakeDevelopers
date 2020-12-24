67d789679e111994946bea0c9c51bcb4b43f759a
bearbearbear <bear-bear-bear@kakao.com>
2020-12-24 15:49:31 +0900

    fix: 이전 클래스명 변경에 따라 css와 js를 바뀐 클래스명에 맞게 수정

--- frontend/src/es6/common/post/postWrite/postWrite.js
+++ frontend/src/es6/common/post/postWrite/postWrite.js
@@ -1,8 +1,21 @@
-const postForm = document.body.querySelector('.post-write__form');
-const postSubmit = document.body.querySelector('.post-write__submit');
+const postWrite = document.body.querySelector('.post-write');
 
-const blockClick = () => {
-  postSubmit.disabled = true;
+const blockDragFileExecution = (e) => {
+  e.preventDefault();
 };
 
-postForm.addEventListener('submit', blockClick, false);
+const blockClick = (e) => {
+  const submit = postWrite.querySelector('.post-write__submit__btn');
+
+  submit.disabled = true;
+};
+
+const postWriteInit = () => {
+  const form = postWrite.querySelector('.post-write__form');
+  const fileBox = postWrite.querySelector('.post-write__file__box');
+
+  form.addEventListener('submit', blockClick, false);
+  fileBox.addEventListener('change', blockDragFileExecution, false);
+};
+
+document.addEventListener('DOMContentLoaded', postWriteInit, false);
