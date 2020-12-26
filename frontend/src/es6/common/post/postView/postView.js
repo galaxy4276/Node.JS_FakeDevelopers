ca69e623905adab401e5d7fa4de862354d0f8863
galaxy <galaxyhi4276@gmail.com>
2020-12-26 10:33:46 +0900

    feat: 프런트엔드 댓글 실시간 반영 js 작성 ( postView.js )

--- frontend/src/es6/common/post/postView/postView.js
+++ frontend/src/es6/common/post/postView/postView.js
@@ -1,20 +1,61 @@
-const commentView = document.querySelector('.post-view__comment_data');
+const commentView = document.querySelector('div.post-view__comment__data');
 const sumbitBtn = document.querySelector('.post-view__comment-btn');
-const referrer = document.querySelector('.referrer');
-const data = document.querySelector('.post-view__comment-textarea').textContent;
+const referrer = document.querySelector('.referrer').textContent;
 
-console.log(`data: ${data}`);
-console.log(`referrer: ${referrer}`);
+console.dir(commentView);
+
+const getDate = () => {
+
+}
+
+const addCommentBrowser = comment => {
+  console.log('프론트에 댓글 추가 됨.');
+  const commentBody = document.createElement('div');
+  commentBody.classList.add('post-view__comment__body');
+  commentBody.style.display = 'flex';
+  commentBody.style.flexDirection = 'column';
+
+  /* --- commentBody 종속적 요소들 --- */
+  const commentInfoDiv = document.createElement('div');
+  const commentContentDiv = document.createElement('div');
+
+  const userSpan = document.createElement('span');
+  userSpan.innerText = comment.UserId;
+
+  const createdAtSpan = document.createElement('span');
+  createdAtSpan.innerText = comment.createdAt;
+
+  /* --- commentContentDiv 는 댓글 내용 --- */
+  /* --- commentInfoDiv 는 댓글 정보 (사용자, 생성일자 등 ), append 요소들 --- */
+  commentInfoDiv.appendChild(userSpan);
+  commentInfoDiv.appendChild(createdAtSpan);
+
+  commentContentDiv.innerText = comment.comment;
+
+  /* --- commentBody append 요소들 --- */
+  commentBody.appendChild(commentInfoDiv);
+  commentBody.appendChild(commentContentDiv);
 
 
+  /* --- 결과 --- */
+  commentView.appendChild(commentBody);
+};
 
-const addCommentServer = () => {
-  fetch('http://localhost:8001/', {
+const addCommentServer = async () => {
+  const data = document.querySelector('.post-view__comment-textarea').value;
+
+  const comment = await fetch(`http://localhost:8001${referrer}/comment`, {
     method: 'POST',
-    data
+    mode: 'cors',
+    body: JSON.stringify({ data }),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
   })
     .then(res => res.json())
     .catch(err => console.error(err));
+  console.log(comment);
+  addCommentBrowser(comment);
 }
 
 const init = () => {
