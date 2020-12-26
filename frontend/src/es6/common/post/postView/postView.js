9fa5f3beaaa58c14a68fcc31a6723b49a6be9588
bearbearbear <bear-bear-bear@kakao.com>
2020-12-26 15:19:35 +0900

    refactor: 서버측에서 변경한 댓글 기능 관련 프론트 코드 주제넘은 리팩터링

--- frontend/src/es6/common/post/postView/postView.js
+++ frontend/src/es6/common/post/postView/postView.js
@@ -1,65 +1,57 @@
-const commentView = document.querySelector('div.post-view__comment__data');
-const sumbitBtn = document.querySelector('.post-view__comment-btn');
-const referrer = document.querySelector('.referrer').textContent;
+const commentList = document.querySelector('.post-view__comment__list');
 
-console.dir(commentView);
+//TODO: 시간 가공하기
+const getDate = () => {};
 
-const getDate = () => {
-
-}
-
-const addCommentBrowser = comment => {
+const addCommentBrowser = (commentInfo) => {
   console.log('프론트에 댓글 추가 됨.');
-  const commentBody = document.createElement('div');
-  commentBody.classList.add('post-view__comment__body');
-  commentBody.style.display = 'flex';
-  commentBody.style.flexDirection = 'column';
 
-  /* --- commentBody 종속적 요소들 --- */
-  const commentInfoDiv = document.createElement('div');
-  const commentContentDiv = document.createElement('div');
+  const DOMfragment = document.createDocumentFragment();
 
-  const userSpan = document.createElement('span');
-  userSpan.innerText = comment.UserId;
+  const commentItem = document.createElement('div');
+  commentItem.classList.add('post-view__comment__item');
 
-  const createdAtSpan = document.createElement('span');
-  createdAtSpan.innerText = comment.createdAt;
+  //TODO: 내부 엘리먼트들 클래스 추가하기
+  commentItem.innerHTML = `
+<div>
+  <span>${commentInfo.UserId}</span>
+  <span>${commentInfo.createdAt}</span>
+</div>
+<div>${commentInfo.comment}</div>
+`.trim();
 
-  /* --- commentContentDiv 는 댓글 내용 --- */
-  /* --- commentInfoDiv 는 댓글 정보 (사용자, 생성일자 등 ), append 요소들 --- */
-  commentInfoDiv.appendChild(userSpan);
-  commentInfoDiv.appendChild(createdAtSpan);
+  DOMfragment.append(commentItem);
 
-  commentContentDiv.innerText = comment.comment;
-
-  /* --- commentBody append 요소들 --- */
-  commentBody.appendChild(commentInfoDiv);
-  commentBody.appendChild(commentContentDiv);
-
-
-  /* --- 결과 --- */
-  commentView.appendChild(commentBody);
+  commentList.append(DOMfragment);
 };
 
-const addCommentServer = async () => {
-  const data = document.querySelector('.post-view__comment-textarea').value;
+const postComment = async () => {
+  const para = document.querySelector('.post-view__comment__textarea').value;
+  const reqUrl = `${document.URL}/comment`;
 
-  const comment = await fetch(`http://localhost:8001${referrer}/comment`, {
+  console.log(`작성내용 => ${para}`);
+  console.log(`요청 API => ${reqUrl}`);
+
+  const commentInfo = await fetch(reqUrl, {
     method: 'POST',
     mode: 'cors',
-    body: JSON.stringify({ data }),
+    body: JSON.stringify({ data: para }),
     headers: {
-      'Content-Type': 'application/json; charset=utf-8'
+      'Content-Type': 'application/json; charset=utf-8',
     },
   })
-    .then(res => res.json())
-    .catch(err => console.error(err));
-  console.log(comment);
-  addCommentBrowser(comment);
-}
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
 
-const init = () => {
-  sumbitBtn.addEventListener('click', addCommentServer);
-}
+  console.log(commentInfo);
 
-init();
+  addCommentBrowser(commentInfo);
+};
+
+const postViewInit = () => {
+  const sumbitBtn = document.querySelector('.post-view__comment__btn');
+
+  sumbitBtn.addEventListener('click', postComment, false);
+};
+
+document.addEventListener('DOMContentLoaded', postViewInit, false);
