94edec22a1e1c206d23d29968c604205241ea9f6
bearbearbear <bear-bear-bear@kakao.com>
2020-12-31 23:29:13 +0900

    feat: 로그인, 로그아웃 닫기 버튼 클릭시 기존 입력값 삭제하도록 설정

--- frontend/src/es6/common/auth/_authToggleEventHandle.js
+++ frontend/src/es6/common/auth/_authToggleEventHandle.js
@@ -1,32 +1,44 @@
-/* --- */
-/* 로그인, 회원가입 창 열고닫기  */
+const clearAuthInputs = () => {
+  const inputs = document.querySelectorAll('.js-authInput');
 
-const loginOpenBtns = document.querySelectorAll('.js-loginOpenBtn'); // 로그인 열기 버튼
-const loginCloseBtn = document.querySelector('.login__form__close-btn'); // 로그인 닫기 버튼
-const loginWindow = document.querySelector('.login'); // 로그인 창
-
-const joinOpenBtns = document.querySelectorAll('.js-joinOpenBtn'); // 회원가입 열기 버튼
-const joinCloseBtn = document.querySelector('.join__form__close-btn'); // 회원가입 닫기 버튼
-const joinWindow = document.querySelector('.join'); // 회원가입 창
+  inputs.forEach((input) => (input.value = ''));
+};
 
 const toggleloginWindow = () => {
+  const loginWindow = document.querySelector('.login'); // 로그인 창
+
   loginWindow.classList.toggle('login--show');
 };
 
 const togglejoinWindow = (e) => {
+  clearAuthInputs();
+
   let currentBtn = e.target;
   let isBtnInLoginWindow = currentBtn.classList.contains('login__form__join-open-btn');
 
   if (isBtnInLoginWindow) toggleloginWindow(); // join 버튼이 로그인 창 안에 있는 버튼이라면 로그인 창 닫기
 
+  const joinWindow = document.querySelector('.join'); // 회원가입 창
+
   joinWindow.classList.toggle('join--show');
 };
 
+const handleLoginClose = () => {
+  clearAuthInputs();
+  toggleloginWindow();
+};
+
 const authToggleEventHandle = () => {
+  const loginOpenBtns = document.querySelectorAll('.js-loginOpenBtn'); // 로그인 열기 버튼
+  const loginCloseBtn = document.querySelector('.login__form__close-btn'); // 로그인 닫기 버튼
+
   loginOpenBtns.forEach((loginOpenBtn) => {
-    loginOpenBtn.addEventListener('click', toggleloginWindow, false);
+    loginOpenBtn.addEventListener('click', handleLoginClose, false);
   });
-  loginCloseBtn.addEventListener('click', toggleloginWindow, false);
+  loginCloseBtn.addEventListener('click', handleLoginClose, false);
+
+  const joinOpenBtns = document.querySelectorAll('.js-joinOpenBtn'); // 회원가입 열기 버튼
+  const joinCloseBtn = document.querySelector('.join__form__close-btn'); // 회원가입 닫기 버튼
 
   joinOpenBtns.forEach((joinOpenBtn) => {
     joinOpenBtn.addEventListener('click', togglejoinWindow, false);
