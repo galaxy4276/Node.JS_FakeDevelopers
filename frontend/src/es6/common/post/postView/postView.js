68bb838583d5ba56b23f779a5b78ee32e1e8b3ae
bearbearbear <bear-bear-bear@kakao.com>
2021-01-01 20:27:25 +0900

    feat: 로컬호스트(개발환경)에서 이미지를 불러올 수 없을을 알리는 안내 modal 설정

--- frontend/src/es6/common/post/postView/postView.js
+++ frontend/src/es6/common/post/postView/postView.js
@@ -1,5 +1,6 @@
 import postNewComment from './_postNewComment';
 import ckeckDelete from './_ckeckDelete';
+import modal from '../../components/_modal';
 
 const postViewInit = () => {
   // 댓글 작성
@@ -13,6 +14,16 @@ const postViewInit = () => {
   const postDeleteBtn = document.querySelector('.post-view__footer__btn--delete');
 
   if (postDeleteBtn) postDeleteBtn.addEventListener('click', ckeckDelete, false);
+
+  // 이미지가 있지만 개발모드라 불러오지 못할 때, 안내 메세지.
+  if (process.env.NODE_ENV === 'development') {
+    const contentBox = document.querySelector('.post-view__content');
+    const isImage = contentBox.querySelector('output').dataset.isImageForLocalhost === 'true';
+
+    if (!isImage) return;
+
+    modal(() => {}, '⚙\n‼ 로컬호스트 환경 ‼\n이미지를 불러올 수 없습니다.');
+  }
 };
 
 document.addEventListener('DOMContentLoaded', postViewInit, false);
