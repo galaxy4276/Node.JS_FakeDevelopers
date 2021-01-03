530a9cab971037224ccd94e210e90c07dcfe1a3e
bearbearbear <bear-bear-bear@kakao.com>
2021-01-04 00:09:11 +0900

    style: 로딩 관련 함수 주석 추가

--- frontend/src/es6/common/function/_loadingBeforePageLoad.js
+++ frontend/src/es6/common/function/_loadingBeforePageLoad.js
@@ -1,28 +1,26 @@
 import loading from '../components/_loading';
 
-const verifyLoading = (elem) => {
+const isLoadingElem = (elem) => {
   if (!elem) return;
 
   const vaildElem = ['SUBMIT', 'BUTTON', 'A'];
   const vaildClassWord = ['auth', 'index', 'post'];
 
+  // 엘리먼트 검증
   const tagName = elem.tagName;
+  if (!vaildElem.includes(tagName)) return;
 
-  if (!vaildElem.includes(tagName)) return false;
-
+  // 클래스네임 검증
   const className = elem.className;
-
   if (!className) return false;
-
   const isVaildClass = vaildClassWord.find((word) => className.includes(word));
-
-  if (!isVaildClass) return false;
+  if (!isVaildClass) return;
 
   return true;
 };
 
 const handleBeforePageLoad = (e) => {
-  if (!verifyLoading(e.target)) return;
+  if (!isLoadingElem(e.target)) return;
 
   window.addEventListener('beforeunload', loading.on, false);
 };
