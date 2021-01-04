39d284cb6e687fe8518cadc5a148a32530d02b87
bearbearbear <bear-bear-bear@kakao.com>
2021-01-05 03:22:41 +0900

    feat: 현재 카테고리에 해당하는 nav 색 강조하는 기능 추가

--- frontend/src/es6/common/common.js
+++ frontend/src/es6/common/common.js
@@ -4,6 +4,7 @@ import './auth/_auth.js';
 /* -- common templates -- */
 import './templates/_sidebar';
 import './templates/_header';
+import './templates/_subMain';
 
 /* -- loading before pageLoad -- */
 import './function/_loadingBeforePageLoad';
---
+++ frontend/src/es6/common/templates/_subMain.js
@@ -0,0 +1,36 @@
+const warnEmpty = (name) => {
+  if (!!name === false) return;
+
+  console.warn(
+    `'${name}' 이(가) 존재하지 않습니다. index 페이지이거나 헤당 엘리먼트에 변경이 있습니다.`
+  );
+};
+
+const isEmpty = (elem, className = '') => {
+  if (!!elem === false) {
+    warnEmpty(className);
+    return true;
+  } //
+  else return false;
+};
+
+const onNavColor = () => {
+  const utilList = document.querySelector('.sub-contents__util-list');
+
+  if (isEmpty(utilList, '.sub-contents__util-list')) return;
+
+  const currCategory = utilList.lastElementChild.textContent;
+
+  const navList = document.querySelector('.sub-nav__list');
+
+  if (isEmpty(navList, '.sub-nav__list')) return;
+
+  const currNav = Array.from(navList.children).find(
+    (item) => item.firstChild.textContent === currCategory
+  );
+
+  currNav.style.color = 'white';
+  currNav.style.backgroundColor = '#149ab8';
+};
+
+document.addEventListener('DOMContentLoaded', onNavColor, false);
