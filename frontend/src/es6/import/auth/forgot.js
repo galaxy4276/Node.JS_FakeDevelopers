a0414c2900ca8f716229e9f687f9da3e8a3d0600
bearbearbear <bear-bear-bear@kakao.com>
2021-01-20 04:59:30 +0900

    feat: 비밀번호 재설정 페이지 닫기 기능 구현

--- frontend/src/es6/import/auth/forgot.js
+++ frontend/src/es6/import/auth/forgot.js
@@ -1,13 +1,24 @@
 import onFormLabelsRaiseUp from '../../common/function/_onFormLabelsRaiseUp';
 import FormVerification from '../../common/auth/_FormVerification';
 
+const backToIndex = () => {
+  location.href = '/';
+};
+
+const onVerifiction = (form) => {
+  const verification = new FormVerification(form);
+
+  verification.on();
+};
+
 const initForgetPage = () => {
   const findForm = document.querySelector('.find__form');
 
   onFormLabelsRaiseUp(findForm);
+  onVerifiction(findForm);
 
-  const verification = new FormVerification(findForm);
-  verification.on();
+  const closeBtn = document.querySelector('.find__form__close-btn');
+  closeBtn.addEventListener('click', backToIndex, false);
 };
 
 document.addEventListener('DOMContentLoaded', initForgetPage, false);
